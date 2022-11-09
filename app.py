5b90bde8cbdbbd4620ecced09f7cef87cfc147c7
Lucas <luscaalima81@gmail.com>
2022-11-09 13:11:47 -0300

    adjustment

--- app.py
+++ app.py
@@ -7,4 +7,7 @@ app=Flask(__name__)
 def obter_livros():
     return jsonify('LUCAS')
 
+@app.route('/livros2',methods=['GET'])
+def obter_livros2():
+    return jsonify('LUCAS2')
 app.run(port=5000,host='localhost',debug=True)
