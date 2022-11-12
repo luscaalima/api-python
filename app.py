88f5a2a51e2cc9e6c0328c8a8724a767057b1150
Lucas <luscaalima81@gmail.com>
2022-11-12 13:08:10 -0300

    API CORS

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from app_firebase import *
 app=Flask(__name__)
+CORS(app)
 
 link='https://lucas-firebase-default-rtdb.firebaseio.com'
 
