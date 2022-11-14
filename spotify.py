1d56907e7efa26bba9586af4e1c3d839970c4b86
Lucas <luscaalima81@gmail.com>
2022-11-14 16:27:12 -0300

    get spotify

--- spotify.py
+++ spotify.py
@@ -1,28 +1,26 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import requests
 client_id="fbb9a866ad494ad4a92e468687de78b4"
 client_secret="af094ee8f9224eaf9aa5c7c5b5bb8043"
-    
 auth_manager = SpotifyClientCredentials(client_id,client_secret)
-sp = spotipy.Spotify(auth_manager=auth_manager)
-# print(sp.current_user())
-# find album by name
-album = "Maquina do Tempo"
-results = sp.search(q = "album:" + album, type = "album")
-print('results',results)
-print('results',results['albums']['items'][0]['artists'][0]['external_urls']['spotify'])
-# print('results',results['albums'][0])
-# get the first album uri
-# album_id = results['albums']['items'][0]['uri']
-# print(sp.current_user_follow_playlist())
-# playlists = sp.user_playlists('spotify',5)
-# print('playlists',playlists)
-# while playlists:
-#     for i, playlist in enumerate(playlists['items']):
-#         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-#     if playlists['next']:
-#         playlists = sp.next(playlists)
-#     else:
-#         playlists = None    
+# print(auth_manager.get_access_token())
+access_token= auth_manager.get_access_token()
+# print('token',token['access_token'])
+# access_token =token['access_token']
+# print('token',access_token)
 
 
+# BASE_URL = 'https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V'
+BASE_URL = 'https://api.spotify.com'
+token=access_token['access_token']
+
+headers = {'Authorization': "Bearer {}".format(token)}
+auth_response = requests.get(f'{BASE_URL}/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V', headers=headers)
+print(auth_response)
+
+#Obter listas de reprodução do usuário atual
+endpointListas = f'{BASE_URL}/v1/me/playlists'
+print('endpoint',endpointListas)
+listaReprodução = requests.get('https://api.spotify.com/v1/me/playlists', headers=headers)
+print(listaReprodução)
